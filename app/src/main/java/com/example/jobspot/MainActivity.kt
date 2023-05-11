886bb86dc8c5e4fc3d3f846ca4160b03bb74725a
Dilshan Dananjaya <127922094+imashaRasanjalee@users.noreply.github.com>
2023-05-11 09:28:34 +0530

    latest project

--- app/src/main/java/com/example/jobspot/MainActivity.kt
+++ app/src/main/java/com/example/jobspot/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.example.jobspot.jobPost.AddJobActivity
-import com.example.jobspot.jobPost.FetchingActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +16,5 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun addJobView(view: View) {
-        val intent = Intent(this, AddJobActivity::class.java)
-        startActivity(intent)
-    }
 
-    fun searchJobs(view: View) {
-        val intent = Intent(this, FetchingActivity::class.java)
-        startActivity(intent)
-    }
 }
